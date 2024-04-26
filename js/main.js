c0684ff33ba2d848736fc765c1879eed71c12d9b
greasyRooster1 <GreasyRooster1@gmail.com>
2024-04-26 10:02:50 -0700

    wowzers 2!

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-
-if(window.location.href !== "dillonwilson27.github.io") {
-    window.location = "http://www.youtube.com/watch_popup?v=oHg5SJYRHA0";
-}
+//
+// if(window.location.href !== "dillonwilson27.github.io") {
+//     window.location = "http://www.youtube.com/watch_popup?v=oHg5SJYRHA0";
+// }
 
 console.log("task 1");
 for(var i=0;i<=10;i++){
@@ -24,10 +24,37 @@ for(var i=1;i<=100;i++){
 }
 console.log(sum);
 console.log("task 5");
-for(var i=1;i<=10;i++){
+let arr = [3,1,4,1,5];
+for(let i=1;i<=10;i++){
     let total = 1;
-    for(var j=1;j<=i;j++) {
+    for(let j=1;j<=i;j++) {
         total*=j;
     }
     console.log(total);
 }
+console.log("task 6");
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+console.log("task 7");
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr.at(-i));
+}
+console.log("task 8");
+let cubes = [];
+for (let i = 0; i < arr.length; i++) {
+    cubes.push(i*i*i);
+}
+console.log(cubes);
+console.log("task 9");
+let fibs = [1,1];
+for (let i = 1; i < arr.length; i++) {
+    fibs.push(fibs[i-1]+fibs[i]);
+}
+console.log(fibs);
+console.log("task 10");
+let back = [1];
+for (let i = 1; i < arr.length; i++) {
+    back.push(arr.at(-i));
+}
+console.log(back);
