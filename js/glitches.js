8e09c98241fd144122da6738a88b796d24244484
greasyRooster1 <GreasyRooster1@gmail.com>
2024-06-03 09:14:20 -0700

    wip glitches

---
+++ js/glitches.js
@@ -0,0 +1,13 @@
+function createGlitch(){
+    let el = document.createElement("div")
+    el.classList.add("glitch");
+    el.style.top=getRandomInt(100)+"vh";
+    el.style.left=getRandomInt(100)+"vw";
+    el.style.width=getRandomInt(200)+"px";
+    el.style.height=getRandomInt(200)+"px";
+    document.body.appendChild(el);
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
