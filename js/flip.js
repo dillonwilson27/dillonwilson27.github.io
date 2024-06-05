42bac274e983530f68c5df520474f9ca55432635
greasyRooster1 <GreasyRooster1@gmail.com>
2024-06-05 09:43:38 -0700

    rotate

---
+++ js/flip.js
@@ -0,0 +1,6 @@
+const title = document.querySelector(".title-content");
+
+title.addEventListener("click",function () {
+    document.body.style.transform = "rotate3d(1, 2, 2, 103deg)"
+    document.body.style.transition = "5s"
+});
